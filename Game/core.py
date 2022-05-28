9a826238eeb5f56fd6d3d30c08c9655d75352cef
Honza2268 <mrak0020@gmail.com>
2022-05-28 16:43:20 +0200

    pohyb dámy, rework pohybu žetonu, implementace stromu možnách pohybů (spolupráce s ondrou v hovoru)

--- Game/core.py
+++ Game/core.py
@@ -2,27 +2,28 @@ from player import Player
 from pawns import Man, King, Color
 import csv
 
+
 class Game:
     def __init__(self, players, starting_player=1):
         self.players = players
         self.current_player: Player
         self.next_player: Player
-        
+
         self.board = [None] * 64
         self.pieces = []
-    
+
     def new_game(self):
         self.board = [None] * 64
         self.pieces = []
-        
+
         self.current_player = self.player[0]
         self.next_player = self.player[1]
-        
+
         self.pieces = [Man(Color(i//8)) for i in range(16)]
-            
+
         for i, p in enumerate(self.pieces):
-            p.place_on(self.board, (i*2)+1*(i//4%2)+32*(i//8))
-    
+            p.place_on(self.board, (i*2)+1*(i//4 % 2)+32*(i//8))
+
     def promote_piece(self, piece, force=False):
         if piece.position in piece._king_zone or force:
             k = King(piece)
@@ -30,43 +31,49 @@ class Game:
             self.pieces[self.pieces.index(piece)] = k
             return True
         return False
-    
+
     def print_numbers(self): print(self.__str__(numbers=True))
     def print(self): print(self.__str__())
+    def d_print(self): print(self.__str__(numbers=True))
+
     def __str__(self, reverse=True, markings=True, numbers=False):
-        c = [f'\x1B[48;5;{"142" if (p%2)^(p//8%2) else "64"}m{self.board[p] if self.board[p] else "  " if not numbers else f"{p:02}"}\x1B[0m' for p in range(64)]
-        l = [f'{(x+1) if markings else ""} '+''.join(c[x*8:x*8+8]) for x in range(8)]
+        c = [
+            f'\x1B[48;5;{"142" if (p%2)^(p//8%2) else "64"}m{self.board[p] if self.board[p] else "  " if not numbers else f"{p:02}"}\x1B[0m'
+             for p in range(64)]
+        l = [f'{(x+1) if markings else ""} '+''.join(c[x*8:x*8+8])
+             for x in range(8)]
         return '\n'.join((['  A B C D E F G H'] if markings else []) + l[:: -1 if reverse else 1])
-    
+
     def position_to_anotation(self, position):
         nta = {1: 'a', 2: 'b', 3: 'c', 4: 'd', 5: 'e', 6: 'f', 7: 'g', 8: 'h'}
-        col = nta[position%8+1]
-        row = str((position//8)+1)    
+        col = nta[position % 8+1]
+        row = str((position//8)+1)
         return col+row
-        
+
     def anotation_to_position(self, anotation):
         atn = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8}
         col = atn[anotation[0]]-1
         row = int(anotation[1])-1
         return row*8+col
-    
+
     def save(self, filename):
         with open(filename, 'w', newline='') as f:
             writer = csv.writer(f, delimiter=',')
             for p in self.pieces:
-                writer.writerow((self.position_to_anotation(p.position), ('w' if p.color else 'b') * (1 if type(p) == Man else 2)))
-    
+                writer.writerow((self.position_to_anotation(
+                    p.position), ('w' if p.color else 'b') * (1 if type(p) == Man else 2)))
+
     def load(self, filename, starting_player_index=None):
         self.board = [None] * 64
         self.pieces = []
-        
+
         if type(starting_player_index) == int:
-            self.current_player = self.player[starting_player_index]
-            self.next_player = self.player[starting_player_index-1]
+            self.current_player = self.players[starting_player_index]
+            self.next_player = self.players[starting_player_index-1]
         else:
-            self.current_player = self.player[0]
-            self.next_player = self.player[1]
-        
+            self.current_player = self.players[0]
+            self.next_player = self.players[1]
+
         with open(filename, 'r') as f:
             reader = csv.reader(f, delimiter=',')
             for row in reader:
@@ -75,11 +82,19 @@ class Game:
                 p = Man(Color(0 if 'b' in piece else 1))
                 self.pieces.append(p)
                 p.place_on(self.board, position)
-                if len(piece) == 2: self.promote_piece(p, True)
-            
-            
+                if len(piece) == 2:
+                    self.promote_piece(p, True)
+    
+    def get_piece_moves(self, piece, strict=False):
+        return piece.get_moves(strict=strict)
+    
+    def make_move(self, piece, move_id):
+        piece.vis_moves()
+
+
 if __name__ == "__main__":
     g = Game((Player(Color(0)), Player(Color(1))))
-    g.load('test1.csv')
-    g.print()
-    print(g.pieces[0].get_moves())
+    g.load('../test2.csv')
+    g.d_print()
+    g.make_move(g.pieces[0], 0)
+    # print(g.pieces[0].get_moves())
--- Game/pawns.py
+++ Game/pawns.py
@@ -1,62 +1,107 @@
 from enum import Enum, IntEnum
 from abc import ABC, abstractmethod
+from treelib import Node, Tree
+
+WHITE_SPOTS = list((i)+1*(i//8 % 2) for i in range(0, 64, 2))
+
 
 class Color(IntEnum):
     BLACK = 0
     WHITE = 1
-  
+
 
 class Color_code(Enum):
     BLACK = '\x1B[38;5;16m'
     WHITE = '\x1B[38;5;15m'
-    
-    
+
+
 class Moves(Enum):
     BLACK = [7, 9]
     WHITE = [-9, -7]
-    KING = [v*i for i in range(1, 8) for v in (7, 9, -9, -7)]
-    
+    KING = [7, 9, -9, -7]  # [v*i for i in range(1, 8) for v in (7, 9, -9, -7)]
+
 
 class King_zone(Enum):
-    BLACK = range(48,64)
+    BLACK = range(48, 64)
     WHITE = range(16)
 
 
 class Piece(ABC):
-    @abstractmethod
-    def __init__(self, color: Color, position = None, board = None):
+    def __init__(self, color: Color, position=None, board=None):
         self.color = color
         self.position: int = position
         self._board: list = board
         self._moves: list
-        self._taken = False
+        self._captured = False
         self._text_color = Color_code[self.color.name]
-            
-    def get_moves(self, pos=None, taken=[]):
+
+    @abstractmethod
+    def get_moves_p1(self, pos, direction, n_pos, used):...
+    
+    @abstractmethod
+    def get_moves_p1(self, pos, direction, n_pos, used):...
+
+    def get_moves(self, pos=None, directions=None, used=None, strict=False):
         moves = []
-        
-        if self._taken: return moves
-        
-        if not pos:
+
+        if not used:
+            used = []
+
+        if self._captured:
+            return [('captured')]
+
+        if pos == None:
             pos = self.position
             moves.append(('start', pos))
+
+        if not directions:
+            directions = self._moves
+        if type(directions) != list:
+            directions = [directions]
+
+        for direction in directions:
+            n_pos = pos + direction
+
+            if not 63 > n_pos > 0 or (n_pos - direction) // 8 == n_pos // 8 or abs(n_pos % 8 - pos % 8) > 1:
+                # pokud je pozice s offsetem kroku kladná, za přelomem desky nebo není na platném poli, pokračuj na další
+                # tldr: detekuje a skipuje nelegální pozice
+                continue
+
+            if not self._board[n_pos] and n_pos not in used:
+                # pokud není pozice s offsetem kroku zabraná, přidej si ji do možných kroků
+                moves.append(self.get_moves_p1(pos, direction, n_pos, used))
+
+            elif self._board[n_pos].color != self.color and n_pos not in used:
+                # pokud je na pozici kroku nepřátelská figurka, zaber ji, pokud je za ní místo
+                moves.append(self.get_moves_p2(pos, direction, n_pos, used))
+
+        if strict:
+            ...
+            #TODO: odstranění neuplatnitelných tahů podle pravidel
         
-        for move in self._moves:
-            try:
-                if pos + move < 0 or pos + move not in ((i)+1*(i//8%2) for i in range(0, 64, 2)) or abs((pos + move) % 8 - pos % 8) != self._moves.index(move)//4+1:
-                    # pokud je pozice s offsetem kroku kladná, za přelomem desky nebo není na platném poli, pokračuj na další
-                    continue
-                
-                if not self._board[pos + move]:
-                    # pokud není pozice s offsetem kroku zabraná, přidej si ji do možných kroků
-                    moves.append(('move', move, pos + move))
-                    
-                elif self._board[pos + move].color != self.color:
-                    ... # TODO: zabírání
-            except IndexError: pass
         return moves
-        
-        
+
+    def vis_moves(self):
+        # stolen from stackoverflow (https://stackoverflow.com/questions/64713797/visualizing-parse-tree-nested-list-to-tree-in-python)
+        lst = self.get_moves()
+        root, *tail = lst
+        tree = Tree()
+        node = Node(root)
+        tree.add_node(node)
+
+        q = [[node, *tail]]
+        while q:
+            parent, *children = q.pop()
+            for child in children:
+                if isinstance(child, list):
+                    head, *tail = child
+                    node = tree.create_node(head, parent=parent)
+                    q.append([node, *tail])
+                else:
+                    tree.create_node(child, parent=parent)
+        tree.show()
+
+
 class Man(Piece):
     def __init__(self, color: Color):
         super().__init__(color)
@@ -70,15 +115,47 @@ class Man(Piece):
             self._board[position] = self
             return True
         return False
-    
+
     def __repr__(self):
         return self._text_color.value+'a '
-    
-    
+
+    def get_moves_p1(self, pos, direction, n_pos, used):
+        return [('move', direction, n_pos)]
+
+    def get_moves_p2(self, pos, direction, n_pos, used):
+        if n_pos+direction < 64 and not self._board[n_pos+direction] and n_pos not in used:
+            sub_move = []
+            sub_move.append(('take', direction, n_pos))
+            sub_move.append(*self.get_moves(
+                pos=n_pos, directions=direction))
+            return sub_move
+
+
 class King(Piece):
     def __init__(self, man: Man):
         super().__init__(man.color, man.position, man._board)
         self._moves = Moves.KING.value
-    
+
     def __repr__(self):
         return self._text_color.value+'b '
+
+    def get_moves_p1(self, pos, direction, n_pos, used):
+        sub_move = []
+        sub_move.append(('move', direction, n_pos))
+        n_move = self.get_moves(
+            pos=n_pos, directions=direction, used=used)
+        if n_move:
+            sub_move.append(*n_move)
+        return sub_move
+
+    def get_moves_p2(self, pos, direction, n_pos, used):
+        if n_pos+direction < 64 and not self._board[n_pos+direction]:
+            used += [n_pos, n_pos+direction]
+            sub_move = []
+            sub_move.append(('take', direction, n_pos))
+            sub_move.append([('move', direction, n_pos+direction)])
+            n_move = self.get_moves(
+                pos=n_pos+direction, used=used)
+            if n_move:
+                sub_move[-1] += n_move
+            return sub_move
